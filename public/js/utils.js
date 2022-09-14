688c4aad631199fc40a2f1d4bbddbcb8581f783f
Kinley Rabgay <80943717+kinley2-glitch@users.noreply.github.com>
2022-09-14 18:22:04 +0600

    Commit
    This is a simple web application developed using ExpressJS and TensorflowJS - deploying pre-trained modal for Japanese and Hongkhong sing language.

---
+++ public/js/utils.js
@@ -0,0 +1,26 @@
+export const removeChild = (inputClass) => {
+  return new Promise((resolve) => {
+    const parent = document.querySelector(inputClass);
+
+    let child = parent.lastElementChild;
+    while (child) {
+      console.log("child: ", child);
+      parent.removeChild(child);
+      child = parent.lastElementChild;
+    }
+    resolve("finished");
+  });
+};
+
+export const checkArrayMatch = (a, b) => {
+  const z = a.map((item) => {
+    return JSON.stringify(item);
+  });
+  return z.includes(JSON.stringify(b));
+};
+
+export const isMobile = () => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+};
